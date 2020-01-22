076f9d44ea0068b4f3be32392e6d09952ac2819c
masa2946 <n.syt.m2946@gmail.com>
2020-01-22 11:16:26 +0900

    alert_fix

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -88,7 +88,7 @@ $(function(){
       }
     })
     .fail(function() {
-      console.log('error');
+      alert("error");
     });
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
